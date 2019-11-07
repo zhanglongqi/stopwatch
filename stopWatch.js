56e22801e961689784b578fd1575b28d9f042ef2
longqi <longqi90@gmail.com>
2019-11-07 13:23:39 +0800

    add total seconds, adjust UI

--- stopWatch.js
+++ stopWatch.js
@@ -9,15 +9,13 @@ var savedTime;
 var paused = 0;
 var running = 0;
 
-function startTimer(){
-  if(!running){
+function startTimer() {
+  if (!running) {
     startTime = new Date().getTime();
     tInterval = setInterval(getShowTime, 1);
     paused = 0;
     running = 1;
-    timerDisplay.style.background = "#FF0000";
     timerDisplay.style.cursor = "auto";
-    timerDisplay.style.color = "yellow";
     startTimerButton.classList.add('lighter');
     pauseTimerButton.classList.remove('lighter');
     startTimerButton.style.cursor = "auto";
@@ -25,15 +23,14 @@ function startTimer(){
   }
 }
 
-function pauseTimer(){
-  if (!difference){
+function pauseTimer() {
+  if (!difference) {
     // if timer never started, don't allow pause button to do anything
   } else if (!paused) {
     clearInterval(tInterval);
     savedTime = difference;
     paused = 1;
     running = 0;
-    timerDisplay.style.background = "#A90000";
     timerDisplay.style.color = "#690000";
     timerDisplay.style.cursor = "pointer";
     startTimerButton.classList.remove('lighter');
@@ -45,15 +42,13 @@ function pauseTimer(){
   }
 }
 
-function resetTimer(){
+function resetTimer() {
   clearInterval(tInterval);
   savedTime = 0;
   difference = 0;
   paused = 0;
   running = 0;
-  timerDisplay.innerHTML = 'Start Studying!';
-  timerDisplay.style.background = "#A90000";
-  timerDisplay.style.color = "#fff";
+  timerDisplay.innerHTML = 'HH:MM:SS:MS<br>Total Seconds';
   timerDisplay.style.cursor = "pointer";
   startTimerButton.classList.remove('lighter');
   pauseTimerButton.classList.remove('lighter');
@@ -61,12 +56,12 @@ function resetTimer(){
   pauseTimerButton.style.cursor = "auto";
 }
 
-function getShowTime(){
+function getShowTime() {
   updatedTime = new Date().getTime();
-  if (savedTime){
+  if (savedTime) {
     difference = (updatedTime - startTime) + savedTime;
   } else {
-    difference =  updatedTime - startTime;
+    difference = updatedTime - startTime;
   }
   // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
   var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -74,9 +69,11 @@ function getShowTime(){
   var seconds = Math.floor((difference % (1000 * 60)) / 1000);
   var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
 
+  var total_seconds = Math.floor(difference / 1000);
+
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
   milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
-  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds + '<br>'+ total_seconds;
 }
